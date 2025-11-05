3938d1a10dc6037b9b6c326212ea62085ec495a1
2500018056-prog <2500018056@webmail.uad.ac.id>
2025-11-05 10:16:51 +0700

    Update Tugas ddp1 Hafiz.cpp

--- Tugas ddp1 Hafiz.cpp
+++ Tugas ddp1 Hafiz.cpp
@@ -8,15 +8,16 @@ int main(){
 	cout << "Masukan Bilangan Angka Satuan Detik :";
 	cin  >> detik;
 	
-	jam=(detik%86400)/3600;
-	menit=(detik%3600)/60;
-	jumlahkeseluruhandetik=detik%60;
+	jam=(detik%86400)/3600; // Sisa setelah hari dibagi 3600
+	menit=(detik%3600)/60;  // Sisa setelah jam dibagi 60
+	jumlahkeseluruhandetik=detik%60; // Sisa detik terakhir
 	
     cout <<endl;
     cout << "Hasil konversi Dari Jam , Menit , Detik :" <<endl;
 	cout << detik <<" "<< "detik" <<" "<< "=" <<" "<< jam << " " << "jam" << " " << menit << " " << "menit" << " " << jumlahkeseluruhandetik << " " << "detik" <<endl;
 	
 }
+
 
 
 
